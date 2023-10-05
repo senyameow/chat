c61b154214d8237c63a46d30abda45f8787a8e93
senyameow <arseniyxd@gmail.com>
2023-10-05 16:26:04 +0700

    sidebar + routes

---
+++ app/(site)/(main)/(routes)/_components/NavigationSidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SidebarRoutes from './SidebarRoutes'
+
+
+
+const NavigationSidebar = async () => {
+
+    return (
+        <div className='space-y-2 h-full border-r px-2 pt-2 pb-4'>
+            <div className='flex items-center space-y-2 flex-col'>
+                <SidebarRoutes />
+            </div>
+        </div>
+    )
+}
+
+export default NavigationSidebar
---
+++ app/(site)/(main)/(routes)/_components/SidebarRoutes.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+
+import { ActionTooltip } from '@/components/ActionTooltip'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { ArrowLeftFromLine, MessageCircle, Users } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const SidebarRoutes = () => {
+
+    const pathname = usePathname()
+
+    return (
+        <>
+            <ActionTooltip label='messages' side='right'>
+                <Link href={'/conversations'} className={cn(`${buttonVariants({ variant: 'ghost' }), 'py-3 px-3 rounded-lg'}`, (pathname === '/users' || pathname.startsWith('/users')) && `bg-gray-100`)}>
+                    <MessageCircle className='w-6 h-6 text-blue-500' />
+                </Link>
+            </ActionTooltip>
+            <ActionTooltip label='people' side='right'>
+                <Button onClick={() => { }} className={`py-3 px-3 rounded-lg`} variant={'ghost'}>
+                    <Users className='w-6 h-6 text-lime-500' />
+                </Button>
+            </ActionTooltip>
+            <ActionTooltip label='leave' side='right'>
+                <Button onClick={() => { }} className={`py-3 px-3 rounded-lg`} variant={'ghost'}>
+                    <ArrowLeftFromLine className='w-6 h-6 text-rose-500' />
+                </Button>
+            </ActionTooltip>
+        </>
+    )
+}
+
+export default SidebarRoutes
