794814b92d69a5d5be59e7cdeb88cad9b7761a08
senyameow <arseniyxd@gmail.com>
2023-10-05 17:59:57 +0700

    actions to get current logged in user

---
+++ actions/get-current-user.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db"
+import getSession from "./get-session"
+
+
+export const getCurrentUser = async () => {
+    try {
+        const session = await getSession()
+
+        if (!session?.user?.email) return null
+
+        // мы могли сделать вот так: return session.user, но нам это не надо, т.к. по дефолту там слишком мало даты (почта имя картинка)
+        // нам нужно больше инфы, но нам все равно нужна эта сессия потому что она дает что? правииильно уникальную дату - email
+        // теперь по этому емэйлу мы можем обратиться к дбшке к табличке с юзерками и найти нашего юзера, и у нас будет гораздо больше инфы про него, которая нам понадобится
+
+        return await db.user.findUnique({
+            where: {
+                email: session.user.email
+            },
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
